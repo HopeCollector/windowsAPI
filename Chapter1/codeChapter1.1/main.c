05c31454ea74d0c119f0a08eb5555518c523f265
Hope Collector <hunter4187@163.com>
2016-08-16 13:28:25 +0800

    fix more bugs in girignore

--- Chapter1/codeChapter1.1/main.c
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
-{
-	MessageBox(NULL, TEXT("Hello world"), TEXT("Hello"), 0);
-
-	return 0;
-}
